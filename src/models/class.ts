db892ae9e000bd15dd03978148b6ff5e166784e7
yaninandiaz <yaninandiaz@gmail.com>
2024-11-03 10:54:08 -0300

    update user schema and add class schema

---
+++ src/models/class.ts
@@ -0,0 +1,52 @@
+import { Model, DataTypes, Optional } from 'sequelize';
+import sequelize from "../database/index";
+
+interface ClassAttributes {
+  id: number;
+  name: string;
+  startDate: Date;
+  endDate: Date;
+}
+
+interface ClassCreationAttributes extends Optional<ClassAttributes, 'id'> {}
+
+class Class extends Model<ClassAttributes, ClassCreationAttributes> implements ClassAttributes {
+  public id!: number;
+  public name!: string;
+  public startDate!: Date;
+  public endDate!: Date;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Class.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    startDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    endDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+
+    }
+  },
+  {
+    sequelize,
+    modelName: 'Class',
+    tableName: 'classes',
+    timestamps: true,
+  }
+);
+
+export default Date;
--- src/services/user_service.ts
+++ src/services/user_service.ts
@@ -1,7 +1,6 @@
-import { response } from "express";
-import { User } from "../models/user"
 import { GeneralError } from "../errors/general_error";
 import { createToken } from "../utils/token";
+import User from "../models/user";
 
 class UserService {
 
@@ -13,7 +12,7 @@ class UserService {
         })
     }
 
-    async create(newUser: any): Promise<{ user: InstanceType<typeof User>, token: string } | null> {
+    async create(newUser: any): Promise<{ user: User, token: string } | null> {
         const savedUser = await this.getByemail(newUser.email)
         if (savedUser) {
             throw new GeneralError(400, "User already exists")
@@ -26,15 +25,15 @@ class UserService {
         return { user, token }
     }
 
-    async getByemail(email: string): Promise<InstanceType<typeof User> | null> {
+    async getByemail(email: string): Promise<User | null> {
         return await User.findOne({ where: { email: email } })
     }
 
-    async getById(idUserToFind: number): Promise<InstanceType<typeof User> | null> {
+    async getById(idUserToFind: number): Promise<User | null> {
         return await User.findByPk(idUserToFind);
     }
 
-    async getAll(): Promise<InstanceType<typeof User>[] | null> {
+    async getAll(): Promise<User[] | null> {
         return await User.findAll();
     }
 }
